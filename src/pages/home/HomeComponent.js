a2a5e76d5b171d4f1d62d8515d1d23dd55393890
vikash.patel1 <p.vikash2810@gmail.com>
2021-07-27 11:13:19 +0530

    added homepage to my portfolio

--- src/pages/home/HomeComponent.js
+++ src/pages/home/HomeComponent.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/header/header";
+import Greeting from "../../containers/greeting/Greeting";
 
 const Home = ({ theme }) => {
   return (
     <div>
       <Header theme={theme} />
+      <Greeting theme={theme} />
     </div>
   );
 };
--- src/portfolio.js
+++ src/portfolio.js
@@ -12,12 +12,65 @@ const seo = {
 const greeting = {
   title: "Vikash Patel",
   logo_name: "VikashPatel",
-  nickname: "",
+  nickname: "Full-Stack Developer",
   subTitle:
     "A passionate individual who always thrives to work on end to end products which develop sustainable and scalable social and technical systems to create impact.",
-  resumeLink:
-    "",
+  resumeLink: "",
   portfolio_repository: "",
 };
 
-export { greeting, seo };
+const socialMediaLinks = [
+  /* Your Social Media Link */
+  // github: "https://github.com/ashutosh1919",
+  // linkedin: "https://www.linkedin.com/in/ashutosh-hathidara-88710b138/",
+  // gmail: "ashutoshhathidara98@gmail.com",
+  // gitlab: "https://gitlab.com/ashutoshhathidara98",
+  // facebook: "https://www.facebook.com/laymanbrother.19/",
+  // twitter: "https://twitter.com/ashutosh_1919",
+  // instagram: "https://www.instagram.com/layman_brother/"
+
+  {
+    name: "Github",
+    link: "https://github.com/p-vikash2810",
+    fontAwesomeIcon: "fa-github", // Reference https://fontawesome.com/icons/github?style=brands
+    backgroundColor: "#181717", // Reference https://simpleicons.org/?q=github
+  },
+  {
+    name: "LinkedIn",
+    link: "https://www.linkedin.com/in/vikash-patel-0a09441a0/",
+    fontAwesomeIcon: "fa-linkedin-in", // Reference https://fontawesome.com/icons/linkedin-in?style=brands
+    backgroundColor: "#0077B5", // Reference https://simpleicons.org/?q=linkedin
+  },
+  // {
+  //   name: "YouTube",
+  //   link: "https://www.youtube.com/channel/UC_amoXmmxSY9KusoDczDTXQ",
+  //   fontAwesomeIcon: "fa-youtube", // Reference https://fontawesome.com/icons/youtube?style=brands
+  //   backgroundColor: "#FF0000", // Reference https://simpleicons.org/?q=youtube
+  // },
+  {
+    name: "Gmail",
+    link: "mailto:uniquevikashpatel@gmail.com",
+    fontAwesomeIcon: "fa-google", // Reference https://fontawesome.com/icons/google?style=brands
+    backgroundColor: "#D14836", // Reference https://simpleicons.org/?q=gmail
+  },
+  // {
+  //   name: "Twitter",
+  //   link: "https://twitter.com/ashutosh_1919",
+  //   fontAwesomeIcon: "fa-twitter", // Reference https://fontawesome.com/icons/twitter?style=brands
+  //   backgroundColor: "#1DA1F2", // Reference https://simpleicons.org/?q=twitter
+  // },
+  {
+    name: "Facebook",
+    link: "https://www.facebook.com/vikash.patel.37454/",
+    fontAwesomeIcon: "fa-facebook-f", // Reference https://fontawesome.com/icons/facebook-f?style=brands
+    backgroundColor: "#1877F2", // Reference https://simpleicons.org/?q=facebook
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/uniquevikash/",
+    fontAwesomeIcon: "fa-instagram", // Reference https://fontawesome.com/icons/instagram?style=brands
+    backgroundColor: "#E4405F", // Reference https://simpleicons.org/?q=instagram
+  },
+];
+
+export { greeting, seo, socialMediaLinks };
